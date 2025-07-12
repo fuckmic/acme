7eb47ef9f48931ea5c87b385e1c4b28b4ed2da79
fuckmic <ChristopherGonzalez4389595@gmail.com>
2025-07-12 17:38:31 +0200

    -c locales

--- utils/locales.js
+++ utils/locales.js
@@ -7,3 +7,22 @@ export const DE_DE = { code: 'de-DE', tz: `Europe/Berlin`, currency: `EUR`, lang
 export const NL_BE = { code: 'nl-BE', tz: `Europe/Brussels`, currency: `EUR`, lang: `Nederlands (België)`, };
 // 法语 - 比利时
 export const FR_BE = { code: 'fr-BE', tz: `Europe/Brussels`, currency: `EUR`, lang: `Français (Belgique)`, };
+
+// 'sg-SG': {code:'sg-SG' ,tz: `Asia/Singapore`, currency: `SGD`, lang: `English (Singapore)`, }, // 新加坡
+// 'ms-MY': {code:'ms-MY', tz: `Asia/Kuala_Lumpur`, currency: `MYR`, lang: `Malay (Malaysia)`, }, // 马来西亚
+// 'en-GB': {code:'en-GB', tz: `Europe/London`, currency: `GBP`, lang: `English (United Kingdom)`, }, // 英国
+// 'en-IN': {code:'en-IN', tz: `Asia/Kolkata`, currency: `INR`, lang: `English (India)` }, // 印度英语
+
+// 'tr-TR': { code:'tr-TR', tz: `Europe/Istanbul`, currency: `TRY`, lang: `Türkçe (Türkiye)`, }, // 土耳其 土耳其语
+// 'fr-FR': {code:'fr-FR' , tz: `Europe/Paris`, currency: `EUR`, lang: `Français (France)`,}, // 法国 法语
+// 'th-TH': {code:'th-TH', tz: `Asia/Bangkok`, currency: `THB`, lang: `ภาษาไทย (ประเทศไทย)`,}, // 泰国 泰语
+// 'ru-RU': { code: 'ru-RU', tz: `Europe/Moscow`, currency: `RUB`, lang: `Русский (Россия)`, }, // 俄罗斯 俄语
+// 'vi-VN': {code:'vi-VN', tz: `Asia/Ho_Chi_Minh`, currency: `VND`, lang: `Tiếng Việt (Việt Nam)`, }, // 越南 越语
+// 'pt-BR': {code:'pt-BR', tz: `America/Sao_Paulo`, currency: `BRL`, lang: `Português (Brasil)`, }, // 巴西 葡萄牙语
+// 'ja-JP': { code: 'ja-JP', tz: `Asia/Tokyo`, currency: `JPY`, lang: `日本語 (日本)`, },
+// 'zh-HK': { code: 'zh-HK', tz: `Asia/Hong_Kong`, currency: `HKD`, lang: `中文 (香港)`, }, // 香港
+
+// =========   以下为 红涨绿跌地区    =========
+// 'ko-KR': { code: 'ko-KR', tz: `Asia/Seoul`, currency: `KRW`, lang: `한국어 (대한민국)`, },
+// 'zh-TW': { code: 'zh-TW', tz: `Asia/Taipei`, currency: `TWD`, lang: `中文 (台灣)` },
+// 'zh-CN': {code:'zh-CN', tz: `Asia/Shanghai`, currency: `CNY`, lang: `中文 (中国)` },
