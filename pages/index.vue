6e4d2187cc2ec3cd09f0545bd933682ff18e93be
fuckmic <ChristopherGonzalez4389595@gmail.com>
2025-07-14 20:27:25 +0200

    -c elevation

--- pages/index.vue
+++ pages/index.vue
@@ -97,6 +97,41 @@
 
 			<view style="margin: 16rpx 0; border: 1rpx dashed var(--acme-fmt-color);"></view>
 			<AcmeEmptyData icon="empty_data" path="/static/" :size="360" format="png" title="Empty Data" />
+
+
+			<view v-for="(v,k) in [0,1,2,3,4,5,6,7,8,9]" :key="k" style="    background-color: #121212;
+    color: #fff;
+    -webkit-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    border-radius: 4px;
+    box-shadow: var(--acme-shadow-0);
+    background-image: var(--acme-overlay-0); 
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 60px;
+    letter-spacing: 0.01071em;
+    text-align: center;
+    color: rgba(255, 255, 255, 0.7);margin-bottom: 40rpx;
+    height: 60px;" :style="{boxShadow:`var(--acme-shadow-${v})`,backgroundImage:`var(--acme-overlay-${v})`}">
+				{{`--acme-shadow-`+v}}
+			</view>
+
+			<view v-for="(v,k) in [0,1,2,3,4,5,6,7,8,9]" :key="k" style="background-color: #fff;
+    color: rgba(0, 0, 0, 0.87);
+    -webkit-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    border-radius: 4px;
+    box-shadow: var(--acme-shadow-0); 
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 60px;
+    letter-spacing: 0.01071em;
+    text-align: center;
+    color: rgba(0, 0, 0, 0.6);margin-bottom: 40rpx;
+    height: 60px;" :style="{boxShadow:`var(--acme-shadow-${v})`}">
+				{{`--acme-shadow-`+v}}
+			</view>
+
 		</view>
 
 	</AcmePageContainer>
